774e82bf932e2dcaeb11f7f78e2eabc7ba9e75f0
Sarthak Choudhari <choudharisarthak21@gmail.com>
2026-03-27 19:38:13 +0530

    test commit

---
+++ Arathmaticoper.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter the two number"<<endl;
+    cin>>a>>b;
+    cout<<"Addition is :"<<a+b<<endl;
+    cout<<"subraction is :"<<a-b<<endl;
+    cout<<"Multiplication is :"<<a*b<<endl;
+    cout<<"Dividion is :"<<a/b<<endl;
+    cout<<"modulas is :"<<a%b<<endl;
+    
+}
