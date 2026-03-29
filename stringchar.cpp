2c91518abbb749c5efa0ca8d7ed191d288db9915
Sarthak Choudhari <choudharisarthak21@gmail.com>
2026-03-29 23:55:16 +0530

    first commit

---
+++ stringchar.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ string FName;
+ cout<<"Enter First Name and Last Name :"<<endl;
+ cin>>FName;
+ cout<<"Total characters in you Name is :"<<FName.size();
+
+}
