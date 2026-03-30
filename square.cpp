655dc089b0f27afd33dc3768f77b0dc8e0f44577
Sarthak Choudhari <choudharisarthak21@gmail.com>
2026-03-30 19:28:15 +0530

    first commit

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    cout<<sqrt(100)<<endl;
+    cout<<round(1.14234)<<endl;
+    cout<<floor(1.7321234)<<endl;
+    cout<<log(5);
+
+}
